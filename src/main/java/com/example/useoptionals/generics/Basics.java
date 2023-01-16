6f478ef9da72c643f8c62f27f8e40e63e1e8a662
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2023-01-16 11:40:28 -0600

    Chore: basic idea about generics

---
+++ src/main/java/com/example/useoptionals/generics/Basics.java
@@ -0,0 +1,31 @@
+package com.example.useoptionals.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Basics {
+    public static void main(String[] args) {
+        List list = new ArrayList<>(5);
+        list.add("Colorado");
+        list.add("Texas");
+        list.add("Arkansas");
+        list.add(23);
+
+        String bestState = (String) list.get(2); // casting to Sting at run time which is expensive
+        System.out.println(bestState);
+
+        String bestState1 = (String) list.get(3); // this error is not found in compie time at all
+        System.out.println(bestState1);
+
+
+        // Use generics to avoid this situation:
+
+        List<String> list2 = new ArrayList<>(5);
+        list2.add("Colorado");
+        list2.add("Texas");
+        list2.add("Arkansas");
+
+        String bestState2 = list2.get(0); // casting to Sting not necessary
+        System.out.println(bestState2);
+    }
+}
